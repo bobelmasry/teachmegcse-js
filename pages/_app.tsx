0672edd36d65a49ceee7e67b546a9fe0470921c1
Aly Hassan <alyhassan2254@gmail.com>
2023-04-19 16:22:09 +0200

    added GA to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../contexts/ThemeContext.js';
+import Script from 'next/script.js';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-    return( 
+    return(
+      <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-HN33M7M3RV"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+      
+          gtag('config', 'G-HN33M7M3RV');
+        `}
+      </Script>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
+      </>
     )
 }
