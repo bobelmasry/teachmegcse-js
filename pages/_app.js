8f96ee4c1a00124271a0b31089dd407aa2c67b2d
Aly Hassan <alyhassan2254@gmail.com>
2023-05-25 11:52:05 +0300

    changed app.tsx to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import '../styles/globals.css';
-import { useState } from 'react'
+import '../styles/globals.css'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { AppProps } from 'next/app'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  initialSession: Session,
-}>) {
+function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
 
   return (
