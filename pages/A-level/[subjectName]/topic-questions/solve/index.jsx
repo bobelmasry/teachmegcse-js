0d26dfe5e7d115c88afeccd0286c2dcdcb540b6b
Aly Hassan <alyhassan2254@gmail.com>
2023-06-13 19:59:47 +0300

    fixed the number of questions in topic questions

--- pages/A-level/[subjectName]/topic-questions/solve/index.jsx
+++ pages/A-level/[subjectName]/topic-questions/solve/index.jsx
@@ -73,7 +73,7 @@ import data2 from "public/all.json"
       <div className="grid grid-rows-4 gap-11 mt-16 mb-24 w-10/12 md:w-5/12 lg:w-3/12">
         {chapters.map((topic) => { 
           const solvedPaper = questionsSolved?.filter(question => question.Chapter.toString() === topic.id.toString());
-          const chapterQuestions = data2.filter(question => (question.Chapter.toString() === topic.id.toString()));
+          const chapterQuestions = data2.filter(question => ((question.Chapter.toString() === topic.id.toString()) && (question.Subject.toString() === str)));
           let amountSolved = Array.isArray(solvedPaper) ? solvedPaper.length : 0
 
           let totalAmount = chapterQuestions.length
