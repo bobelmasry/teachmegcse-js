bdea4090c6b013971263a0e84e4604dc28a7dfbc
bobelmasry <alyhassan2254@gmail.com>
2023-07-18 10:42:33 +0300

    fixed notes aspect ratio and fixed error in 25 mdx and 33

--- pages/A-level/[subjectName]/revision-notes/index.jsx
+++ pages/A-level/[subjectName]/revision-notes/index.jsx
@@ -8,27 +8,38 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { supabase } from 'utils/supabase';
 
-function TopicCard2({linkSrc, header, hasSignIn, hasGrey, hasGreen}) {
-
+function TopicCard2({ linkSrc, header, hasSignIn, hasGrey, hasGreen }) {
   return (
     <div>
-    <Link href={`${linkSrc}`}>
-    <div className="btn flex gap-8 shadow-[0_7px_0_0px_rgb(3,105,161)] md:hover:scale-[1.02] ease-out transition-all rounded block p-6 bg-gray-50 border border-gray-200 rounded-lg shadow md:hover:bg-gray-100 dark:bg-slate-600 dark:border-gray-600 md:dark:hover:bg-gray-500">
-  <h5 className="text-3xl font-semibold text-gray-900 dark:text-white">{header}</h5>
-  {(hasSignIn) && 
-    <h5 className="text-xl mt-2 font-bold text-gray-900 dark:text-white">Sign In to track your progress</h5>
-  }
-  {(hasGrey) && 
-    <Image src={"/checkmark.png"} width={40} height={40} alt="" />
-  }
-  {(hasGreen) && 
-    <Image src={"/correct.png"} width={40} height={40} alt="" />
-  }
-  </div>
-  </Link>
+      <Link href={`${linkSrc}`}>
+        <div className="btn flex gap-8 shadow-[0_7px_0_0px_rgb(3,105,161)] md:hover:scale-[1.02] ease-out transition-all rounded block p-6 bg-gray-50 border border-gray-200 rounded-lg shadow md:hover:bg-gray-100 dark:bg-slate-600 dark:border-gray-600 md:dark:hover:bg-gray-500">
+          <h5 className="text-3xl font-semibold text-gray-900 dark:text-white">{header}</h5>
+          {(hasSignIn) && 
+            <h5 className="text-xl mt-2 font-bold text-gray-900 dark:text-white">Sign In to track your progress</h5>
+          }
+          <div style={{ position: 'relative', width: '56px', height: '42px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {(hasGrey) && 
+              <Image
+                fill 
+                src="/checkmark.png"
+                alt="Current Image" 
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            }
+            {(hasGreen) && 
+              <Image
+                fill 
+                src="/correct.png"
+                alt="Current Image" 
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            }
+          </div>
+        </div>
+      </Link>
     </div>
-  )
-};
+  );
+}
 
 export default function Home({ posts }) {
   const session = useSession()
