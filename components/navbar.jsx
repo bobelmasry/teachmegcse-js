4b8c67ae96e5bcbe686dd3666504dd4114113a66
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2023-09-28 15:35:30 +0300

    added favicon

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar({ session }) {
   const router = useRouter();
@@ -21,9 +21,9 @@ export default function Navbar({ session }) {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-around mx-auto p-4">
         <Link className="flex items-center" href="/">
           <Image
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="./logo.svg"
             className="h-8 mr-3"
-            alt="Flowbite Logo"
+            alt="Logo"
             height={50}
             width={50}
           />
@@ -58,14 +58,14 @@ export default function Navbar({ session }) {
           {!session ? (
             <Link
               className="ml-2 text-white transition-all ease-out bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-500 dark:focus:ring-green-800"
-              href={'/login-or-signup'}
+              href={"/login-or-signup"}
             >
               Sign In
             </Link>
           ) : (
             <Link
               className="text-white transition-all ease-out bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-500 dark:focus:ring-red-800"
-              href={'/login-or-signup'}
+              href={"/login-or-signup"}
             >
               Sign Out
             </Link>
@@ -73,19 +73,19 @@ export default function Navbar({ session }) {
         </div>
         <div
           className={`items-center justify-between ${
-            isMenuOpen ? 'flex' : 'hidden'
+            isMenuOpen ? "flex" : "hidden"
           } w-full md:flex md:w-auto md:order-1`}
           id="navbar-sticky"
         >
           <ul
             className={`${
-              isMenuOpen ? 'flex' : 'hidden'
+              isMenuOpen ? "flex" : "hidden"
             } flex flex-col text-xl p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-600`}
           >
             <li>
               <Link
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 transition-all dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
-                onClick={() => handleNavigate('/IGCSE')}
+                onClick={() => handleNavigate("/IGCSE")}
                 href="/IGCSE"
               >
                 IGCSE
@@ -94,21 +94,21 @@ export default function Navbar({ session }) {
             <li>
               <Link
                 className="block transition-all py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                onClick={() => handleNavigate('/A-level')}
+                onClick={() => handleNavigate("/A-level")}
                 href="/A-level"
               >
                 A-level
               </Link>
               <Link
                 className="block transition-all py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                onClick={() => handleNavigate('/about')}
+                onClick={() => handleNavigate("/about")}
                 href="/about"
               >
                 About Us
               </Link>
               <Link
                 className="block transition-all py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                onClick={() => handleNavigate('/contact')}
+                onClick={() => handleNavigate("/contact")}
                 href="/contact"
               >
                 Contact Us
@@ -120,14 +120,14 @@ export default function Navbar({ session }) {
         <div className="hidden md:flex md:gap-8 md:ml-28 md:items-center md:justify-center">
           <Link
             className="block text-2xl py-2 px-4 font-semibold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 transition-all dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700"
-            onClick={() => handleNavigate('/IGCSE')}
+            onClick={() => handleNavigate("/IGCSE")}
             href="/IGCSE"
           >
             IGCSE
           </Link>
           <Link
             className="block text-2xl py-2 px-4 font-semibold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-            onClick={() => handleNavigate('/A-level')}
+            onClick={() => handleNavigate("/A-level")}
             href="/A-level"
           >
             A-level
