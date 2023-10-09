785d1876230f4e51c13e992af3af1219119b0b46
bobelmasry <alyhassan2254@gmail.com>
2023-10-09 13:18:26 +0300

    shouldve fixed physics

--- pages/IGCSE/[subjectName]/topic-questions/core/list/index.jsx
+++ pages/IGCSE/[subjectName]/topic-questions/core/list/index.jsx
@@ -68,7 +68,7 @@ import { useUser } from '@supabase/auth-helpers-react'
       <div className="grid grid-rows-4 gap-8 mt-16 mb-24 w-auto">
       {chapters.map((topic) => { 
           const solvedPaper = questionsSolved?.filter(question => ((question.Chapter.toString() === topic.id.toString()) && (question.Subject.toString() === str) && (question.Level?.toString() === 'IGCSE')));
-          const chapterQuestions = data2.filter(question => ((question.Chapter.toString() === topic.id.toString()) && (question.Subject.toString() === str)));
+          const chapterQuestions = data2.filter(question => ((question.Chapter.toString() === topic.id.toString()) && (question.Subject.toString() === str) && (question.Level.toString() === 'IGCSE')));
           let amountSolved = Array.isArray(solvedPaper) ? solvedPaper.length : 0
           let totalAmount = chapterQuestions.length
 
