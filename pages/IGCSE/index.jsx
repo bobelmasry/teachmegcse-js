2709687587319da1da004250481f0e729a7b39d1
bobelmasry <alyhassan2254@gmail.com>
2023-10-10 07:57:42 +0300

    added IG links

--- pages/IGCSE/index.jsx
+++ pages/IGCSE/index.jsx
@@ -23,10 +23,10 @@ export default function Home() {
     <div className="flex justify-center items-center">
       <div className="grid grid-flow-row gap-16 mt-24 sm:mt-36 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 sm:justify-center mb-40 w-11/12 md:w-4/6 lg:w-4/6">
         <SubjectCard header={"Business (0450)"}  link1={"/IGCSE/business/"} link2={"#"} link3={"#"} />
-        <SubjectCard header={"Biology (0610)"} link1={"/IGCSE/biology/"} link2={"#"} link3={"#"} />
-        <SubjectCard header={"Chemistry (0620)"} link1={"/IGCSE/chemistry/"} link2={"#"} link3={"#"} />
+        <SubjectCard header={"Biology (0610)"} topicQuestions={true} link1={"/IGCSE/biology/"} link2={"#"} link3={"/IGCSE/biology/topic-questions"} />
+        <SubjectCard header={"Chemistry (0620)"} topicQuestions={true} link1={"/IGCSE/chemistry/"} link2={"#"} link3={"/IGCSE/chemistry/topic-questions"} />
         <SubjectCard header={"Computer Science (0478)"} link1={"/IGCSE/computer-science/"} link2={"#"} link3={"#"} />
-        <SubjectCard header={"Economics (0455)"} link1={"/IGCSE/economics/"} link2={"#"} link3={"#"} />
+        <SubjectCard header={"Economics (0455)"} topicQuestions={true} link1={"/IGCSE/economics/"} link2={"#"} link3={"/IGCSE/economics/topic-questions/extended"} />
         <SubjectCard header={"English as a First Language (0500)"} link1={"/IGCSE/english-language/"} link2={"#"} link3={"#"} />
         <SubjectCard header={"History (0470)"} link1={"/IGCSE/history/"} link2={"#"} link3={"#"} />
         <SubjectCard header={"Maths (0580)"} link1={"/IGCSE/math/"} link2={"#"} link3={"#"}  />
