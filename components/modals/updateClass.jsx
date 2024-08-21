18e98d63a89bf811de0dea6d16bd6b59e377ffae
bobelmasry <alyhassan2254@gmail.com>
2024-08-21 16:30:33 +0300

    added gap

--- components/modals/updateClass.jsx
+++ components/modals/updateClass.jsx
@@ -106,7 +106,7 @@ export default function UpdateClass ({classID, subject, school, level, userID}) 
       </form>
     </ModalBody>
 
-<ModalFooter>
+<ModalFooter gap={4}>
 <button onClick={event => updateClass(event)}
         className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
       >
